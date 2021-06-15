cf027a4d2c69c5e97453ea75875883e8589ca3a6
tizianasordini <tizianasordini@hotmail.com>
2021-06-15 18:49:11 -0300

    listados de la index

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 window.addEventListener("load", function(){
     let listadoArtistas = document.querySelector(".listadoArtistas")
+    let listadoDiscos = document.querySelector(".listadoDiscos")
+    let listadoCanciones = document.querySelector(".listadoCanciones")
     let urlArtistas = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart"
     
     fetch(urlArtistas)
@@ -8,16 +10,38 @@ window.addEventListener("load", function(){
             return respuesta.json()
          })
         .then(function(datos){
-        // pido los datos que necesito de la URL
+        // pido los datos que necesito de la URL (Artistas-Discos-Canciones)
             console.log(datos)
             let losArtistas = datos.artists.data
-            // la info que llego la pongo dentro de mi codigo html
-            for (let i=0; i<losArtistas.length; i++){
+            let losDiscos = datos.albums.data
+            let lasCanciones = datos.tracks.data
+            // la info que llego de la pongo dentro de mi codigo html
+            // ARISTAS
+            for (let i=0; i<5; i++){
                 listadoArtistas.innerHTML += `
-                <li>
-                <img src="${losArtistas[i].picture}" alt="${losArtistas[i].name}">
-                </li>
+                <figure><img src="${losArtistas[i].picture}" alt="${losArtistas[i].name}"></figure>
+                <div class="textos">
+                <a href="detail-artist.html">${losArtistas[i].name}</a>
+                </div>
                 `}
+            // DISCOS 
+            for (let i=0; i<5; i++){
+                listadoDiscos.innerHTML += `
+                <figure><img src="${losDiscos[i].cover}" alt="${losDiscos[i].title}"></figure>
+                <div class="textos">
+                <a href="detail-album.html">${losDiscos[i].title}</a>
+                <a href="detail-artist.html">${losArtistas[i].name}</a>
+                </div>
+                `}
+                for (let i=0; i<5; i++){
+                    listadoCanciones.innerHTML += `
+                    <figure><img src="${losDiscos[i].cover}" alt="${lasCanciones[i].title}"></figure>
+                    <div class="textos">
+                        <a href="detail-track.html">${lasCanciones[i].title}</a>
+                        <a href="detail-artist.html">${losArtistas[i].name}</a>
+                        <a href="detail-album.html">${losDiscos[i].title}</a>
+                    </div>
+                    `} 
             })        
         .catch(function(error){
             console.log(error)
