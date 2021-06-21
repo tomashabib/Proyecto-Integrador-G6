dee5f34490b5d773a8f616daa6497ba52dec378f
Lucas Orifici <lucas@MacBook-Pro-de-Lucas.local>
2021-06-21 11:55:35 -0300

    modificaciones Playlist y generos

--- js/detail-genres.js
+++ js/detail-genres.js
@@ -1,4 +1,30 @@
+
 window.addEventListener("load", function(){
-
-
-});
+    // donde voy a poner despues la info que traigo
+    let listadoGeneros = document.querySelector(".contenedor generos")
+    // las url de donde traigo la info, para el FETCH
+    let urlGeneros = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/0" 
+    // GENEROS
+    fetch(urlGeneros)
+        .then(function(respuesta){
+            console.log(respuesta)
+            return respuesta.json()
+        })
+        .then(function(datos){
+        // Pido los datos que necesito de la URL (nombre y foto artista)
+            console.log(datos)
+            let artistas = datos.data
+            // la info que llego de la API la pongo dentro de mi codigo html
+            // GENEROS
+            for (let i=0; i<5; i++){
+                listadoGeneros.innerHTML += `
+                <div class="textos">
+                <figure><img src="${Genero[i].picture}" alt="${Genero[i].name}"></figure>
+                <a href="detail-genres.html?id=${genero[i].id}">${genero[i].name}</a>
+                </div>
+            `}
+        })        
+        .catch(function(error){
+            console.log(error)
+        })
+        });
--- js/playlist.js
+++ js/playlist.js
@@ -1,4 +1,43 @@
 window.addEventListener("load", function(){
 
+let proxy = 'https://cors-anywhere.herokuapp.com/';
 
-});
+let recuperoStorage = localStorage.getItem('playlist');
+let playlist = JSON.parse(recuperoStorage);
+
+console.log(playlist);
+
+let listaTracks = document.querySelector(".medioplaylist");
+if( playlist.lenght == 0 ) {
+    listaTracks.innerHTML += `
+    <p>Aun no hay canciones en tu Playlist</p>`
+} else{
+    playlist.forEach(function(idTrack) {
+        encontrarTrack(idTrack)
+    });
+}
+function encontrarTrack(idTrack){
+    let url = proxy + "https://api.deezer.com/track/" + idTrack;
+    fetch(url)
+    .then(function(response){
+        return response.json();
+    })
+    .then (function(response){
+        return response.json();
+    })
+    .then (function(tracks){
+        console.log(tracks)
+        let untrack = tracks;
+        let iduntrack= untrack.id;
+        let nombretrack = untrack.title; 
+        listaTracks.innerHTML += `
+        <article>
+            <h3> <a class="decoration" href="detail-track.html?id=${iduntrack}">${nombretrack} </a> </h3>
+        </article>    
+        `
+    })
+    .catch(function(error){
+        console.log(error)
+    })
+    }
+})
