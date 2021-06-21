8f02290460b2cc563d075e46ad1510afa9b12b47
tizianasordini <tizianasordini@hotmail.com>
2021-06-21 12:36:55 -0300

    mejorando detalle de artista

--- js/detail-artist.js
+++ js/detail-artist.js
@@ -1,12 +1,12 @@
 window.addEventListener("load", function(){
     console.log(location.search)
-    // Guardar la qs
+    // Guardar la qs // Id del artista
     const parametros = new URLSearchParams(location.search)
     const cual = parametros.get("id")
     console.log(cual)
     // llamar a  la seccion del artista y segunda seccion donde estan sus canciones
-    let tituloArtista = document.querySelector(".tituloartist")
-    let elArtista = document.querySelector(".contenedorartista")
+    let tituloArtista = document.querySelector(".tituloartist a")
+    let elArtista = document.querySelector(".contenedorartista img")
     let cancionesArtista = document.querySelector(".botonesCanciones")
     // la url que me provee la API
     let urlArtista = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${cual}`
@@ -20,32 +20,20 @@ window.addEventListener("load", function(){
         .then(function(datos){
             console.log(datos)
             // Le pido a la API 
-            // PRIMERA PARTE :nombre del artista, imagen del artista y ¿no hay texto de cada artista?
+            // PRIMERA PARTE :nombre del artista y imagen del artista 
             let nombre = datos.name
             console.log(nombre)
             let foto = datos.picture
-        /*    let canciones = datos.tracklist.data  */
             console.log(foto)
-            // lo pongo en el html
-            for (let i=0; i<nombre.length; i++){
-                tituloArtista.innerHTML += `
-                <a>${nombre[i]}</a>
-            `}
-            for (let i=0; i<1; i++){
-                elArtista.innerHTML += `
-                <img src="${foto[i]} alt="${nombre[i]}">
-            `}
-        /*    for (let i=0; i<canciones.length; i++){
-                cancionesArtista.innerHTML += `
-                <li>
-                    <figure><img src="${canciones[i].contributors.picture}"><a href="detail-genres.html">${canciones[i].contributors.name}</a>
-                     </figure>
-                </li>
-            `} */
+            // PRIMERA PARTE : lo pongo en el html
+            // nombre como titulo
+            tituloArtista.innerText = nombre
+            // imagen del artista
+            elArtista.src = foto
+            elArtista.alt = nombre
         })
         .catch(function(error){
             console.log(error)
         })
     
-    // SEGUNDA PARTE:  5 canciones del artista (nombre e imagen de cada cancion)
 });
