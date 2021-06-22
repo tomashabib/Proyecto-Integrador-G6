2f9d8370af10c4e83f540e3d40eda3eb2c1ade6c
tomashabib <thabib@udesa.edu.ar>
2021-06-22 20:09:19 -0300

    Avanzando con Detail Album

--- js/detail-album.js
+++ js/detail-album.js
@@ -1,15 +1,15 @@
 window.addEventListener("load", function(){
-
+    console.log(location.search) 
     // Guardar la qs // Id del Album
     const parametros = new URLSearchParams(location.search)
     const cual = parametros.get("id")
     console.log(cual)
-    
+    // Donde vohy a poner la informacion que traigo
     let tituloArtista = document.querySelector(".tituloartist")
     let listadoDiscos = document.querySelector(".contenedoralbumes")
-
-    let urlAlbumes = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/302127"
-
+    // La URL que me provee los datos
+    let urlAlbumes = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/${cual}`
+    // Hacer el fetch
     fetch(urlAlbumes)
         .then(function(respuesta){
             console.log(respuesta)
@@ -17,7 +17,12 @@ window.addEventListener("load", function(){
         })
         .then(function(datos){
             console.log(datos)
-
+            // Le pido a la API
+            // Primera Parte: Nombre del Artista 
+            let nombre = datos.name
+            console.log(nombre)
+            
+        
         })
 
         .catch(function(error){
