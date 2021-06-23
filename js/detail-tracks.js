52ab034d4ec2f25a013a4d3e2a3ea5c18514c76c
tomashabib <thabib@udesa.edu.ar>
2021-06-23 17:26:44 -0300

    Modificaciones temporarias DT

--- js/detail-tracks.js
+++ js/detail-tracks.js
@@ -4,10 +4,13 @@ console.log(location.search)
     const parametros = new URLSearchParams(location.search)
     const cual = parametros.get("id")
     console.log(cual)
+    
     // Donde voy a poner la informacion que traigo
-    let listadoCanciones = document.querySelector(".contenedor")
+    let listaDeCanciones = document.querySelector (".listaDeCanciones")
+
     // La URL que me provee los datos para el Fetch
     let urlCanciones = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${cual}`
+   
     // Fetch
     fetch(urlCanciones)
         .then(function(respuesta){
@@ -17,6 +20,13 @@ console.log(location.search)
         .then(function(datos){
             console.log(datos)
             // Le pido a la API
+            let nombreAlbum = datos.album.title
+            let imgAlbum = datos.cover_medium
+            let nombreArtista = datos.artist.name
+            let idArtista = datos.artist.id
+            let nombreCancion = datos.title
+
+            // poniendolo en el HTML
 
         })
 
