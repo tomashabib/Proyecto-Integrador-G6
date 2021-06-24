c3483ee23a49077f2722149ee91dad5e201470bc
tomashabib <thabib@udesa.edu.ar>
2021-06-24 01:28:23 -0300

    Detail Genres cambiado por completo y casi terminado

--- js/detail-genres.js
+++ js/detail-genres.js
@@ -1,33 +1,63 @@
-
 window.addEventListener("load", function(){
+    console.log(location.search)
 
-    // donde voy a poner despues la info que traigo
+    // Guardar la QS // Id Generos
+    const parametros = new URLSearchParams(location.search)
+    const cual = parametros.get("id")
+    console.log(cual)
+
+    // Donde voy a poner la infromacion que traigo
     let listadoGeneros = document.querySelector(".contenedor generos")
-    // las url de donde traigo la info, para el FETCH
-    let urlGeneros = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/0" 
+
+    // Las URL que me proveen los datos para el Fetch
+    let urlGeneros = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/${cual}`
+    let urlGenerosArtistas = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/${cual}/artists`
+
     // GENEROS
+    // Fetch
     fetch(urlGeneros)
         .then(function(respuesta){
             console.log(respuesta)
             return respuesta.json()
         })
         .then(function(datos){
-        // Pido los datos que necesito de la URL (nombre y foto artista)
             console.log(datos)
-            let generos = datos.data
-            // la info que llego de la API la pongo dentro de mi codigo html
-            // GENEROS
-            for (let i=0; i<5; i++){
-                listadoGeneros.innerHTML += `
-                <div class="textos">
-                <figure><img src="${Genero[i].picture}" alt="${Genero[i].name}"></figure>
-                <a href="detail-genres.html?id=${genero[i].id}">${genero[i].name}</a>
-                </div>
-            `}
+            // Le pido a la API
+            // Primera Parte: Nombre del Genero
+            let nombreGenero = datos.name
+            console.log(nombreGenero)
+            // Lo pongo en el HTML
+            tituloGenero.innerText = nombreGenero
+
         })        
         .catch(function(error){
             console.log(error)
         })
+
+        // Segunda Parte: Lista de artistas del Genero con su foto y su nombre
+        // Fetch
+        fetch(urlGenerosArtistas)
+        .then(function(respuesta){
+            console.log(respuesta)
+            return respuesta.json()
+        })
+        .then(function(datos){
+            console.log(datos)
+            // Le pido a la API
+            let nombreArtista = datos.name
+            let idArtista = datos.id
+            let fotoArtista = datos.picture_medium
+            // Lo pongo en el HTML
+            
+        })
+            //for (let i=0; i<5; i++){
+            //   listadoGeneros.innerHTML += `
+            //    <div class="textos">
+            //    <figure><img src="${Genero[i].picture}" alt="${Genero[i].name}"></figure>
+            //    <a href="detail-genres.html?id=${genero[i].id}">${genero[i].name}</a>
+            //    </div>
+            //`}
+        
     
     // VALIDACIÓN DE FORMULARIO 
     // capturamos formulario, campo a chequear y lugar donde enviaremos el menasaje
