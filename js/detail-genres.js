f892f689aa8fd9c33821c5a3ba25581c810054cd
tomashabib <thabib@udesa.edu.ar>
2021-06-24 23:49:18 -0300

    Retocando DG

--- js/detail-genres.js
+++ js/detail-genres.js
@@ -7,9 +7,9 @@ window.addEventListener("load", function () {
     console.log(cual)
 
     // Donde voy a poner la infromacion que traigo
-    let tituloGeneros = document.querySelector(".contenedor generos")
-    let imagenArtista = document.querySelector(".contenedor generos img")
-    let nombreArtista = document.querySelector(".contenedor generos a")
+    let tituloGeneros = document.querySelector(".contenedorartista a ")
+    let infoArtista = document.querySelector(".contenedorartista img")
+
 
 
     // Las URL que me proveen los datos para el Fetch
@@ -24,70 +24,76 @@ window.addEventListener("load", function () {
             return respuesta.json()
         })
         .then(function (datos) {
-            console.log(datos)
-            // Le pido a la API
-            // Primera Parte: Nombre del Genero 
-            let nombreGenero = datos.name
-            console.log(nombreGenero)
-            // Lo pongo en el HTML
-            tituloGeneros.innerText = nombreGenero
+                console.log(datos)
+                // Le pido a la API
+                // Primera Parte: Nombre del Genero 
+                let nombreGenero = datos.name
+                console.log(nombreGenero)
+                // Lo pongo en el HTML
+                tituloGeneros.innerText = nombreGenero
 
 
 
-            // Segunda Parte: Lista de artistas del Genero con su foto y su nombre
-            // Fetch
-            fetch(urlGenerosArtistas)
-                .then(function (respuesta) {
-                    console.log(respuesta)
-                    return respuesta.json()
-                })
-                .then(function (datos) {
-                    console.log(datos)
-                    // Le pido a la API
-                    let artistaGenero = datos.data
-                    // Lo pongo en el HTML
-                    for (let i = 0; i < 5; i++) {
-                        nombreGeneros.innerHTML += `
-    <div class="textos">
-        <figure><img src="${artistaGenero[i].picture_medium}" alt="${artistaGenero[i].name}"></figure>
-        <a href="detail-genres.html?id=${artistaGenero[i].id}">${artistaGenero[i].name}</a>
-        </div>
-    `
-                    }
+                // Segunda Parte: Lista de artistas del Genero con su foto y su nombre
+                // Fetch
+                fetch(urlGenerosArtistas)
+                    .then(function (respuesta) {
+                        console.log(respuesta)
+                        return respuesta.json()
+                    })
+                    .then(function (datos) {
+                            console.log(datos)
+                            // Le pido a la API
+                            let nombreArtista = datos.name
+                            console.log(nombreArtista)
+                            let fotoArtista = datos.picture_medium
+                            console.log(fotoArtista)
+                            // Lo pongo en el HTML
+                            infoArtista.src = fotoArtista
+                            infoArtista.alt = nombreArtista
 
-                })
-                .catch(function (error) {
-                    console.log(error)
-                })
+
+                            // for (let i = 0; i < 5; i++) {
+                            //   tituloGeneros.innerHTML += `
+                            //<div class="textos">
+                            // <figure><img src="${artistaGenero[i].picture_medium}" alt="${artistaGenero[i].name}"></figure>
+                            // <a href="detail-genres.html?id=${artistaGenero[i].id}">${artistaGenero[i].name}</a>
+                            // </div>
+                            //  `
+                        })
+
+                    })
+            .catch(function (error) {
+                console.log(error)
+            })
 
         })
 
 
 
-    // VALIDACIÓN DE FORMULARIO 
-    // capturamos formulario, campo a chequear y lugar donde enviaremos el menasaje
-    let formulario = document.querySelector("form")
-    let camboBuscar = document.querySelector("[name=search]")
-    let mensaje = document.querySelector(".alert")
+// VALIDACIÓN DE FORMULARIO 
+// capturamos formulario, campo a chequear y lugar donde enviaremos el menasaje
+let formulario = document.querySelector("form")
+let camboBuscar = document.querySelector("[name=search]")
+let mensaje = document.querySelector(".alert")
 
-    
-    formulario.addEventListener("submit", function (event) {
 
-        // evita cosas predeterminadas. 
-        event.preventDefault();
+formulario.addEventListener("submit", function (event) {
 
-        // si el value esta vacio, que le diga al usurio completar el campo
-        if (camboBuscar.value == "") {
+    // evita cosas predeterminadas. 
+    event.preventDefault();
 
-            mensaje.innerText = "Completar el campo"
+    // si el value esta vacio, que le diga al usurio completar el campo
+    if (camboBuscar.value == "") {
 
-        } else if (camboBuscar.value.length < 3) {
+        mensaje.innerText = "Completar el campo"
 
-            mensaje.innerText = 'Por favor ingrese al menos 3 caracteres a buscar'
+    } else if (camboBuscar.value.length < 3) {
 
-        } else {
-            this.submit()
-        }
+        mensaje.innerText = 'Por favor ingrese al menos 3 caracteres a buscar'
 
-    })
+    } else {
+        this.submit()
+    }
+
 });
